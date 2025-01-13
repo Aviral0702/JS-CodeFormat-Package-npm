a9e91b53ec8a909b4723ece39a938a5d7fe18c3f
Aviral Asthana <aviral.asthana0704@gmail.com>
2025-01-13 12:46:18 +0530

    added support for CLI

---
+++ src/cli.js
@@ -0,0 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import formatCode from '.';
+
+const filePath = process.argv[2];
+if(!filePath){
+    console.error('Please provide a file path');
+    process.exit(1);
+}
+const fullPath = path.resolve(filePath);
+const code = fs.readFileSync(fullPath, 'utf-8');
+const formattedCode = formatCode(code);
+fs.writeFileSync(fullPath, formattedCode);
+
+console.log('Code formatted successfully');
